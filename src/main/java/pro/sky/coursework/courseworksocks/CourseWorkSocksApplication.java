e2156075a1b3f8a2849c79617e788bcf463564f1
Spolpinka <a421243266@gmail.com>
2023-03-13 14:51:11 +0300

    Initial commit

---
+++ src/main/java/pro/sky/coursework/courseworksocks/CourseWorkSocksApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework.courseworksocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkSocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/coursework/courseworksocks/CourseWorkSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework.courseworksocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
